e85a0d957da49a2578e1bdda52feed7730367cfa
fabio henrique <fabiomassucatto0102@gmail.com>
2025-04-14 16:34:49 -0300

    base da combilação

---
+++ zero_time.py
@@ -0,0 +1,36 @@
+#tres bombas tres maneiras de defuzar// cada uma com um tipo de enigma diferente e dependendo da resposta a bomba explode
+class Bomb_one:
+    def __init__(self):
+        pass
+
+
+
+
+
+class Bomb_two:
+    def __init__(self):
+        pass
+
+
+
+
+
+
+class Bomb_tree:
+    def __init__(self):
+        pass
+
+
+
+
+
+def defuse(self,bomb_one):
+    pass
+
+
+def defuse(self,bomb_two):
+
+
+    pass
+def defuse(self,bomb_tree):
+    pass
