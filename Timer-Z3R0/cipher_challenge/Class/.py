d48d8e8340ffd960c33fdee3977f26990ac3b91d
PauloKT <paulohamaral89@gmail.com>
2025-04-20 22:19:29 -0300

    update cypher challenger and create folder arts

--- Timer-Z3R0/cipher_challenge/Class/.py
+++
@@ -1,4 +0,0 @@
-from random import randint
-from time import sleep
-from os import system
-
--- Timer-Z3R0/cipher_challenge/Class/cifra.py
+++ Timer-Z3R0/cipher_challenge/Class/cifra.py
@@ -4,19 +4,19 @@ from os import system
 
 class cifradeCesar:
     def __init__(self):
-        self.palavras_originais = ["bomba", "fiovermelho", "timerzero", "paulo", "guilherme", "fabinho", "dancarino", "emo", "katto", "café"]
-        self.palavras_cifrada = []
+        self.palavras_originais = ["bomba", "katto", "banguela", "gatão", "dancarino", "fiovermelho", "timerzero"]
+        self.palavras_cifradas = []
         self.rotações = []
-        self.gerar_cifras = ()
+        self.gerar_cifras()
 
     def gerar_cifras(self):
-        for palavras in self.palavras_originais:
-            rotação = randint(1, 25)
-            cifra = self.cifrar(palavras, rotação)
-            self.palavras_cifrada.append(cifra)
-            self.rotações.append(rotação)
+        for palavra in self.palavras_originais:
+             rotação = randint(1, 25)
+             cifra = self.cifrar(palavra, rotação)
+             self.palavras_cifradas.append(cifra)
+             self.rotações.append(rotação)
 
-    def transformar(self, texto, deslocamento):
+    def cifrar(self, texto, deslocamento):
         resultado = ''
         for char in texto:
             if ('A' <= char <= 'Z') or ('a' <= char <= 'z'):
@@ -34,8 +34,38 @@ class cifradeCesar:
                 resultado += char
         return resultado
     
-chave = cifradeCesar()
-mensagem_cifrada = ""
-print("Use numeros negativos para decifrar")
+def iniciar_jogo():
+    system('cls')
+    cifra = cifradeCesar
+    tempo_bomba = 10
+    erros = 0
 
-chave.decifrar(mensagem_cifrada)
+    i = randint(0, 9)
+    palavra_cifrada = cifra.palavras_cifradas[i]
+    rotacao_correta = cifra.rotação[i]
+
+    print("========= TIMER Z3R0 =========")
+    print(f"\nA cifra é : {palavra_cifrada}")
+    print("Você precisa descobrir a rotção correta de 1 a 25")
+    print("A cada erro 3 erros, 1 minuto será removido do tempo da bomba")
+    print("==============================\n")
+
+while True:
+    tentativa =  input("Digite o numero de rotações: ")
+
+    if tentativa < 1 or tentativa > 25:
+        print("Digite um número válido!\n")
+
+    tentativa = int(tentativa)
+
+    if tentativa == rotacao_correta:
+        print(f"\nSucesso! A rotação correta er {rotacao_correta}")
+        break
+    else:
+        erros += 1
+        print("Você errou!\n")
+        if erros % 3 == 0:
+            tempo_bomba -= 1
+            print(f"Você perdeu 1 minuto! Tempo restante {tempo_bomba} min.")
+            if tempo_bomba <= 0:
+                print("O tempo acabou!")
